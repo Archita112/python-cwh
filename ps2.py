d75c2613f63bcf122f95d57317c3dffb462e3950
Archita Sugandh <115146251+Archita112@users.noreply.github.com>
2023-10-08 20:00:51 +0530

    Add files via upload

---
+++ ps2.py
@@ -0,0 +1,38 @@
+# this is the solution to problems of practice set of strings
+
+# Write a Python program to display a user-entered name 
+# followed by Good Afternoon using input() function.
+
+# a = input("Enter your name: ")
+# print(a,",Good Afternoon")
+
+# Write a program to fill in a letter template given below 
+# with name and 
+
+# letter = ''' Dear <|NAME|>,
+
+#  You are selected!
+
+# <|DATE|>'''
+# name = input("Enter your name: ")
+# date = input("Enter the date: ")
+# letter = letter.replace("<|NAME|>", name)
+# letter = letter.replace("<|DATE|>", date)
+# print(letter)
+
+# Write a program to detect double spaces in a string.
+
+# s = "HEllo everyone, I am Archita Sugandh"
+# s = s.count(" ")
+# print(s)
+
+# Replace the double spaces from problem 3 with single spaces.
+
+# s = s.replace(" ", "  ")
+# print(s)
+
+# Write a program to format the following letter using escape 
+# sequence characters
+
+letter = "Dear Harry,\n This Python course in nice.\n Thanks!!"
+print(letter)
