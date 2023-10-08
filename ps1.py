551b553cac0e68238a3fde6a7572efea1ddcf44a
Archita Sugandh <115146251+Archita112@users.noreply.github.com>
2023-10-08 18:36:22 +0530

    Add files via upload

---
+++ ps1.py
@@ -0,0 +1,44 @@
+# this is the practice set of variables and datatypes.
+
+# write a program to add two numbers.
+
+a = 5
+b = 6
+c = a + b
+print(c)
+
+# write a python program to find the remainder when divided by 2
+
+d = 6
+e = d / 2
+print(e)
+
+# write a python program to check the type of the variable
+# assigned by using the input() function
+
+f = input("Enter name:")
+print(f)
+print(type(f))
+
+# Use a comparison operator to find out whether a 
+# given variable a is greater than b or not. (Take a=34 and b=80)
+
+g = 34
+h = 80
+i = (g>h)
+print(i)
+
+# Write a Python program to find the average of two numbers entered 
+# by the user
+
+j = int(input("Enter a number:"))
+k = int(input("Enter another number:"))
+l = (j + k)/2
+print(l)
+
+# Write a Python program to calculate the square of a number entered by the user
+
+m = int(input("Enter the number:"))
+print(m)
+n = m * m
+print(n)
